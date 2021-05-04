c7ae87aca9ee500f2b9f49b45229ab9a7f291c23
Gilberto <gilbertosa099@outlook.com>
2021-05-03 21:31:36 -0500

    Arreglo del scroll e implementacion de mediaquerys

--- public/index.js
+++ public/index.js
@@ -2,7 +2,6 @@
 const render=async()=>{
     const data = await fetch('../products.json')
     .then(response=>response.json())
-    console.log(data)
     let template = document.getElementById('plantilla').innerHTML;
     let compile = Handlebars.compile(template);
     let contentHTML = compile(data); 
