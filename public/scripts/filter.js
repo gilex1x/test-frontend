da713c91e913cd6f79425e88b23905ad821793fe
Gilberto <gilbertosa099@outlook.com>
2021-05-05 23:05:04 -0500

    Iniciando con el filtro.

--- public/scripts/filter.js
+++ public/scripts/filter.js
@@ -11,12 +11,21 @@ class Filter{
         let compile = Handlebars.compile(template);
         let contentHTML = compile(this.data); 
         document.getElementById('content').innerHTML += contentHTML;
-    }    
+    }  
+    
+    easyFilter(id){
+        let newData  =[];
+        for(const beer in this.data.products){
+            newData.push([beer,this.data.products[beer]])
+        }
+        console.log(newData);
+    }
 }
 
 const inicializar=async ()=>{
     const data = await fetch('../products.json')
-    .then(response=>response.json()); 
+    .then(response=>response.json());
+    console.log(data); 
     window.filtro = new Filter(data);   
 };
 
