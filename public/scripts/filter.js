9d1e73214dffedb279057f6a1220d303cdbc4b73
Gilberto <gilbertosa099@outlook.com>
2021-05-05 22:36:20 -0500

    Arreglando el llamado a la data

--- public/scripts/filter.js
+++ public/scripts/filter.js
@@ -1,28 +1,23 @@
- 
+
 class Filter{
-    constructor(){
-        this.renderInicial();
+    
+    constructor(data){
+        this.data=data;
+        this.render();
     }
 
-    async getData (){
-        const data = await fetch('../products.json')
-        .then(response=>response.json());
-        return data;
-    }
-
-    async renderInicial(){
-        
+    async render(){  
         let template = document.getElementById('plantilla').innerHTML;
         let compile = Handlebars.compile(template);
-        let contentHTML = compile(this.getData()); 
+        let contentHTML = compile(this.data); 
         document.getElementById('content').innerHTML += contentHTML;
     }    
 }
 
-const inicializar=()=>{
-    window.filtro = new Filter();
-    
+const inicializar=async ()=>{
+    const data = await fetch('../products.json')
+    .then(response=>response.json()); 
+    window.filtro = new Filter(data);   
 };
 
 inicializar();
-
