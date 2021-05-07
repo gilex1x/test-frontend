f73abc072d93b2559cd335d770f880316f7ce17a
Gilberto <gilbertosa099@outlook.com>
2021-05-06 22:10:07 -0500

    Filtro avance

--- public/scripts/filter.js
+++ public/scripts/filter.js
@@ -3,29 +3,33 @@ class Filter{
     
     constructor(data){
         this.data=data;
-        this.render();
+        this.render(this.data);
     }
 
     async render(){  
-        let template = document.getElementById('plantilla').innerHTML;
-        let compile = Handlebars.compile(template);
-        let contentHTML = compile(this.data); 
+         let template = document.getElementById('plantilla').innerHTML;
+         let compile = Handlebars.compile(template);
+         let contentHTML = compile(this.data); 
         document.getElementById('content').innerHTML += contentHTML;
-    }  
-    
-    easyFilter(id){
-        let newData  =[];
-        for(const beer in this.data.products){
-            newData.push([beer,this.data.products[beer]])
-        }
-        console.log(newData);
     }
+
+    handleChange(check){
+        let cards = (this.data.products);
+        console.log(cards);
+        if(check.checked == true){
+            cards.map(beer=>{
+                console.log(beer);
+            })
+        }
+    }
+        
+        
 }
 
 const inicializar=async ()=>{
     const data = await fetch('../products.json')
     .then(response=>response.json());
-    console.log(data); 
+    //console.log(data); 
     window.filtro = new Filter(data);   
 };
 
